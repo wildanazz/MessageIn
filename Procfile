59aa2c47930eab51914d00ea278c3ee93c005106
Muhammad Wildan Aziz <wildanazzwa@gmail.com>
2018-02-20 12:10:03 +0000

    Edit files

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-migrate: bash deployment.sh
 web: gunicorn myanothersite.wsgi --log-file -
--- myanothersite/settings.py
+++ myanothersite/settings.py
@@ -11,11 +11,11 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import dj_database_url
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PRODUCTION = os.environ.get('DATABASE_URL') != None
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -83,10 +83,6 @@ DATABASES = {
     }
 }
 
-
-#If Using Heroku Environemnt, then Use Database Setting on Heroku
-if PRODUCTION:
-    DATABASES['default'] = dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
